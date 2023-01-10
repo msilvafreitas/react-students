1eb6b3f3d8d1029f223f38cc031f34a709829799
matheus <freitas1matheus@gmail.com>
2023-01-10 00:25:37 -0300

    dashboard redesign

--- src/components/dashboard.jsx
+++ src/components/dashboard.jsx
@@ -7,269 +7,49 @@ export function Dashboard() {
                 <p className='font-semibold text-[22px]'>Meus Alunos</p>
                 <input className='px-4 py-2 rounded-[4px] text-sm' type="text" placeholder='Pesquisar alunos' />
             </div>
-            <table className='table-auto w-full gap-2'>
-                <thead className='bg-[#F3F4F6]'>
-                    <tr className='h-14 mb-2'>
-                        <th>Nome do Aluno</th>
-                        <th>Atualização</th>
-                        <th>Av. 1</th>
-                        <th>Av. 2</th>
-                        <th>Av. 3</th>
-                        <th>Av. 4</th>
-                        <th>Média Final</th>
-                        <th>Situação</th>
-                    </tr>
-                </thead>
-                <tbody className='gap-2'>
-                    <tr className='h-16 bg-white px-4'>
-                        <td>Fábio Fernandes</td>
-                        <td>
-                            20-12-2022
-                            22:09
-                        </td>
-                        <td>5.00</td>
-                        <td>8.00</td>
-                        <td>9.00</td>
-                        <td>7.00</td>
-                        <td>7.25</td>
-                        <td>Aprovado</td>
-                    </tr>
-                </tbody>
-            </table>
-            <div class="flex flex-col mt-8">
-            <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
-                <div class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
-                    <table class="min-w-full">
-                        <thead>
-                            <tr>
-                                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Name</th>
-                                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Title</th>
-                                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Status</th>
-                                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50 text-left text-xs leading-4 font-medium text-gray-500 uppercase tracking-wider">Role</th>
-                                <th class="px-6 py-3 border-b border-gray-200 bg-gray-50"></th>
-                            </tr>
-                        </thead>
-                        <tbody class="bg-white">
-                            <tr>
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <div class="flex items-center">
 
+            <div class="flex flex-col">
+                <div class="-my-2 py-2 overflow-x-auto sm:-mx-6 sm:px-6 lg:-mx-8 lg:px-8">
+                    <div class="align-middle inline-block min-w-full shadow overflow-hidden sm:rounded-lg border-b border-gray-200">
+                        <table class="min-w-full">
+                            <thead>
+                                <tr className='h-14'>
+                                    <th class="px-4 py-3 border-b border-gray-200 bg-[#F3F4F6] text-left font-medium">Nome do Aluno</th>
+                                    <th class="px-4 py-3 border-b border-gray-200 bg-[#F3F4F6] text-left font-medium">Atualização</th>
+                                    <th class="px-4 py-3 border-b border-gray-200 bg-[#F3F4F6] text-left font-medium">Av. 1</th>
+                                    <th class="px-4 py-3 border-b border-gray-200 bg-[#F3F4F6] text-left font-medium">Av. 2</th>
+                                    <th class="px-4 py-3 border-b border-gray-200 bg-[#F3F4F6] text-left font-medium">Av. 3</th>
+                                    <th class="px-4 py-3 border-b border-gray-200 bg-[#F3F4F6] text-left font-medium">Av. 4</th>
+                                    <th class="px-4 py-3 border-b border-gray-200 bg-[#F3F4F6] text-left font-medium">Média Final</th>
+                                    <th class="px-4 py-3 border-b border-gray-200 bg-[#F3F4F6] text-left font-medium">Situação</th>
+                                </tr>
+                            </thead>
+                            <tbody class="bg-white">
+                                <tr>
+                                    <td class="px-4 py-3 whitespace-no-wrap border-b border-gray-200 text-[#131313]">John Doe</td>
 
-                                        <div class="ml-4">
-                                            <div class="text-sm leading-5 font-medium text-gray-900">John Doe</div>
-                                            <div class="text-sm leading-5 text-gray-500">john@example.com</div>
-                                        </div>
-                                    </div>
-                                </td>
+                                    <td class="px-4 py-3 whitespace-no-wrap border-b border-gray-200">
+                                        <div class="leading-6 text-gray-900">20-12-2022</div>
+                                        <div class="text-sm leading-5 text-gray-500">22:09</div>
+                                    </td>
 
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
-                                    <div class="text-sm leading-5 text-gray-500">Web dev</div>
-                                </td>
+                                    <td class="px-4 py-3 whitespace-no-wrap border-b border-gray-200 text-[#131313]">5.00</td>
+                                    <td class="px-4 py-3 whitespace-no-wrap border-b border-gray-200 text-[#131313]">8.00</td>
+                                    <td class="px-4 py-3 whitespace-no-wrap border-b border-gray-200 text-[#131313]">9.00</td>
+                                    <td class="px-4 py-3 whitespace-no-wrap border-b border-gray-200 text-[#131313]">7.00</td>
+                                    <td class="px-4 py-3 whitespace-no-wrap border-b border-gray-200 text-[#131313]">7.25</td>
 
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
-                                </td>
+                                    <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
+                                        <span class="px-6 py-[6px] inline-flex leading-5 rounded-full bg-[#BBF7D0] ">Aprovado</span>
+                                    </td>
 
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">Owner</td>
+                                </tr>
 
-                                <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <div class="flex items-center">
-
-
-                                        <div class="ml-4">
-                                            <div class="text-sm leading-5 font-medium text-gray-900">John Doe</div>
-                                            <div class="text-sm leading-5 text-gray-500">john@example.com</div>
-                                        </div>
-                                    </div>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
-                                    <div class="text-sm leading-5 text-gray-500">Web dev</div>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">Owner</td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <div class="flex items-center">
-
-
-                                        <div class="ml-4">
-                                            <div class="text-sm leading-5 font-medium text-gray-900">John Doe</div>
-                                            <div class="text-sm leading-5 text-gray-500">john@example.com</div>
-                                        </div>
-                                    </div>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
-                                    <div class="text-sm leading-5 text-gray-500">Web dev</div>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">Owner</td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <div class="flex items-center">
-
-
-                                        <div class="ml-4">
-                                            <div class="text-sm leading-5 font-medium text-gray-900">John Doe</div>
-                                            <div class="text-sm leading-5 text-gray-500">john@example.com</div>
-                                        </div>
-                                    </div>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
-                                    <div class="text-sm leading-5 text-gray-500">Web dev</div>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">Owner</td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <div class="flex items-center">
-
-
-                                        <div class="ml-4">
-                                            <div class="text-sm leading-5 font-medium text-gray-900">John Doe</div>
-                                            <div class="text-sm leading-5 text-gray-500">john@example.com</div>
-                                        </div>
-                                    </div>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
-                                    <div class="text-sm leading-5 text-gray-500">Web dev</div>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">Owner</td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <div class="flex items-center">
-
-
-                                        <div class="ml-4">
-                                            <div class="text-sm leading-5 font-medium text-gray-900">John Doe</div>
-                                            <div class="text-sm leading-5 text-gray-500">john@example.com</div>
-                                        </div>
-                                    </div>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
-                                    <div class="text-sm leading-5 text-gray-500">Web dev</div>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">Owner</td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <div class="flex items-center">
-
-
-                                        <div class="ml-4">
-                                            <div class="text-sm leading-5 font-medium text-gray-900">John Doe</div>
-                                            <div class="text-sm leading-5 text-gray-500">john@example.com</div>
-                                        </div>
-                                    </div>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
-                                    <div class="text-sm leading-5 text-gray-500">Web dev</div>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">Owner</td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                </td>
-                            </tr>
-                            <tr>
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <div class="flex items-center">
-
-
-                                        <div class="ml-4">
-                                            <div class="text-sm leading-5 font-medium text-gray-900">John Doe</div>
-                                            <div class="text-sm leading-5 text-gray-500">john@example.com</div>
-                                        </div>
-                                    </div>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <div class="text-sm leading-5 text-gray-900">Software Engineer</div>
-                                    <div class="text-sm leading-5 text-gray-500">Web dev</div>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200">
-                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800">Active</span>
-                                </td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap border-b border-gray-200 text-sm leading-5 text-gray-500">Owner</td>
-
-                                <td class="px-6 py-4 whitespace-no-wrap text-right border-b border-gray-200 text-sm leading-5 font-medium">
-                                    <a href="#" class="text-indigo-600 hover:text-indigo-900">Edit</a>
-                                </td>
-                            </tr>
-                        </tbody>
-                    </table>
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
-            </div></div>
-            )
+        </div>
+    )
 }
